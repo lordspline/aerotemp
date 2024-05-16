340c837c0fc709d52feb2c222b6b2a4fc82f387d
lordspline <74811063+lordspline@users.noreply.github.com>
2024-05-16 03:49:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,18 +15,18 @@ function IndexPage() {
       <div className="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto text-center">
         {subscribed ? (
           <h1 className="font-primary font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-snug">
-            You have subscribed!
+            You're all set!
           </h1>
         ) : (
           <div className="space-y-8">
             <h1 className="font-primary font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-tight">
-              Easily capture emails on {' '}
+              Hassle-free cloud gaming on {' '}
               <span className="text-palette-primary">
-                Notion.
+                Aeromancer.
               </span>
             </h1>
             <p className="font-secondary text-palette-light text-base md:text-lg lg:text-xl">
-              Capture your customer emails on a beautiful landing page that syncs with your Notion workspace.
+              Play your favorite games with negligible latency thanks to the power of AI Codecs. Sign up on our waitlist for launch updates.
             </p>
             <Subscribe setSubscribed={setSubscribed} />
           </div>
